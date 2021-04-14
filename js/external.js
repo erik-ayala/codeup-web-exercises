570c64e319e2c8c65648d41213cf577254df7194
Erik Ayala <erikayala@Eriks-MacBook-Pro.local>
2021-04-14 16:13:08 -0500

    pushing out test

---
+++ js/external.js
@@ -0,0 +1,12 @@
+"user strict";
+
+alert("Welcome to my Website");
+
+var userInput = prompt("What is your favorite color?").toUpperCase();
+if (userInput === "Purple".toUpperCase()){
+    alert("Did we just become best friends?")
+    console.log("I'm lying :)")
+} else {
+    alert("That's pretty cool man, but my favorite color is better.")
+    console.log("I don't like this guy.")
+}
