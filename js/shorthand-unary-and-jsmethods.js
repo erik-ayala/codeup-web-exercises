4d0a833e8903529c132fbc017c9b9c6cd9e97a4d
Erik Ayala <erikayala@Eriks-MacBook-Pro.local>
2021-04-15 17:02:40 -0500

    added lessons for today apr 15

---
+++ js/shorthand-unary-and-jsmethods.js
@@ -0,0 +1,72 @@
+"use strict";
+
+// SHORTHAND ASSIGNMENT OPERATORS
+
+var name = "Erik";
+
+// name = name + " " + "Ayala";
+
+name += " Ayala";
+
+console.log(name);
+
+//MINI Exercise
+
+// var result1 = 4
+// console.log(result1 += 8);
+//
+// var result2 = 4
+// console.log(result2 -= 2);
+//
+// var result3 = 4
+// console.log(result3 *= 7);
+//
+// var result4 = 4
+// console.log(result4 /= 2);
+//
+// var result5 = 4
+// console.log(result5 &= .2);
+
+// INCREMENT/DECREMENT
+// post-increment(++)
+
+// var num = 10;
+// console.log(num++);
+// console.log(num);
+
+// post decrement (--)
+
+// JS Built im Methods
+
+// TO STRING
+var num = 5 * 5;
+num = num.toString();
+console.log(typeof num);
+console.log(num);
+
+
+// TO FIXED
+
+var pi = 3.14592
+
+console.log(pi.toFixed(2));
+
+//Number()
+console.log(Number("Erik"));
+
+// isNaN() - checks to see if the value provides is a Not-a-Number
+console.log(isNaN("erik"));
+
+//parseInt()
+console.log(parseInt("377737474848 what a number"));
+
+console.log("Erik".length);
+
+console.log("Erik".indexOf("E"));
+
+
+
+
+
+
+
