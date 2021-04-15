2dad9c2db0b1356a1004516a6e3bcbdf936fd13d
Erik Ayala <ayala.erik61@gmail.com>
2021-04-15 17:02:40 -0500

    added lessons for today apr 15

---
+++ js/java-prim-var-excercises.js
@@ -0,0 +1,69 @@
+// Primitive Type Exercises
+
+console.log(typeof(99.9));
+
+console.log(typeof("false"));
+
+console.log(typeof(false));
+
+console.log(typeof('0'));
+
+console.log(typeof(0));
+
+console.log(typeof(true));
+
+console.log(typeof('true'));
+
+// Operator Exercises
+
+console.log('1' + 2);
+
+console.log(typeof('1' + 2));
+
+console.log(6 % 4);
+
+console.log("3 + 4 is " + (3 + 4));
+
+console.log(0 < 0);
+
+console.log('false' == false);
+
+console.log('true' == true);
+
+console.log(5 >= -5);
+
+console.log(!false || false);
+
+console.log(true || "42");
+
+console.log(!true || !false);
+
+console.log(6 & 5);
+
+console.log(5 < 4 && 1 === 1);
+
+console.log(typeof('codeup' === 'string'));
+
+console.log('codeup' === 'codeup' && 'codeup' === "Codeup");
+
+console.log(4 >= 0 && 1 !== '1');
+
+console.log( 6 % 3 === 0);
+
+console.log(5 & 2 !== 0);
+
+// Working with Data Types, Operators, and Variables Exercises
+
+
+    var a = 1;
+    var b = a++;
+    var c = ++a;
+
+
+
+
+
+
+
+
+
