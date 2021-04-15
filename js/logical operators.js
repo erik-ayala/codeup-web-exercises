bc31371196e486fffd6c0be3c4321174e36861eb
Erik Ayala <ayala.erik61@gmail.com>
2021-04-15 12:21:13 -0500

    Training exercises for today

---
+++ js/logical operators.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// I want a routine wich will determine if a dog is in fact my dog
+// The dog must have for, have whiskers, and have a name
+
+// var hasFur = true;
+// var hasWhiskers = true;
+// var nameIsCoda = true;
+//
+// // Logical &&
+// var isMyDog = (hasFur && hasWhiskers && nameIsCoda);
+//
+// if (isMyDog) {
+//     console.log("Who's a good boy?");
+// }
+
+// Logical OR ||
+// var creditScore = 720;
+// var cashOnHand = 3000;
+//
+// if (creditScore > 600 || cashOnHand > 3000) {
+//     console.log("Congrats! You now own a car!")
+// }
+//
+
+// Logical NOT (!)
+
+console.log("The inverted value is: " + !true)
