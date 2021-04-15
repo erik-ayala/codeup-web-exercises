4c90c802fd79467a0c3430b65418d51d714cdaef
Erik Ayala <erikayala@Eriks-MacBook-Pro.local>
2021-04-15 12:21:13 -0500

    Training exercises for today

---
+++ js/equality-operators.js
@@ -0,0 +1,10 @@
+"user strict";
+
+// LOOSE Equality
+// loose equality (==)
+// Loose inequality (!=)
+
+
+// STRICT Equality
+// STRICT inequality (!==)
+// The comparators must be same data type AND have the same value.
---
+++ js/types-operators.js
@@ -0,0 +1,34 @@
+"use strict";
+
+//a variable is  a symbolic representative of a value that can be different in each run of a program
+// The variable is a pointer to a stored place in memory of your running application
+
+// var isTrue = true;
+// console.log("The value of isTrue: " + isTrue);
+
+// console.log("The number is: " + 10);
+
+// console.log(2 + 2);
+//
+// console.log(6 - 2);
+//
+// console.log(2 * 2);
+//
+// console.log(16 / 4);
+//
+// console.log(25 & 7);
+
+// name = "Laura";
+// \n = space
+// \t = tab
+
+// console.log("\n name")
+// console.log("\t name")
+
+// TYPEOF
+// whenever i need to find out what type something is
+var name;
+console.log("The type is: " + typeof(name));
+
+
+
