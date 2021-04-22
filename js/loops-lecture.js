795a81e0db31931b4999b733a01e2751fbc6a84f
Erik Ayala <ayala.erik61@gmail.com>
2021-04-22 16:58:34 -0500

    pushed todays work/exercises 4 22 2020

---
+++ js/loops-lecture.js
@@ -0,0 +1,86 @@
+//Loops in programing
+
+// Iteration:every loop is an iteration
+//repition of a process to find a answer
+
+//infinite loops: avoid them... will crash browser
+
+
+//While loops
+// while loop will continue to run code until the condition is FALSE
+// be careful with while loops make sure that the condition will meet false
+//checks condition before running code
+
+//SYNTAX:
+//js keyword is "while"
+//condition you are checking against will go inside of these parentheses
+//
+
+// while(/*condition*/) {
+//     // body
+// }
+
+// numOfSteps = 0;
+// while (numOfSteps <= 30) {
+//     console.log('starting iteration at: ${numOfSteps}' );
+//     console.log('keep walking, youre at : ' + numOfSteps + " steps");
+//     numOfSteps = numOfSteps + 10;
+//     console.log("ending iteration at: " + $(numOfSteps));
+//     console.log("--------------------------")
+// }
+
+// // TODO: MINI EXERCISES
+// // TODO: print all even numbers between 0 and 25;
+// // TODO: using prompt() and alert(), write a loop that will continue to ask for the correct password until the
+// // correct password is given.
+// hint: start by defining the correct password and don't forget to link your html file
+
+// var number = 0
+// while (number <= 25) {
+//     if(number % 2 === 0) {
+//         console.log(number);
+//     }
+//     number++;
+// }
+//
+// var userPassword = "codeup123";
+// var userInput = prompt("Enter Password")
+//
+// while (userInput !== userPassword) {
+//     alert('Not the correct password');
+//     userInput = prompt("enter password again.")
+// }
+// alert("enter.")
+
+// var numStudents = 0; //variable holding set of students starting at 0
+// do {
+//     var desksAvailable = 20 -numStudents;
+//     console.log('we have ' + desksAvailable + 'amount of desks left');
+//     numStudents++
+// } while (numStudents <= 20)
+//
+// console.log("sorry this class is full")
+
+//example: sing happy birthday every year and when they reach 16 say "you got a car"
+// variable = age starting point
+
+// var age = 0 //starting point
+//
+// do {
+//     //sing a song
+//     console.log(`happy bday you're now ${age} years old!`)
+//     // increment
+//     age++
+// } while (age <= 16) //
+//
+// // log too old no more celebrations
+// console.log("too old, get a job")
+
+
+// var spaces = 0
+//
+// do {
+//     spaces = spaces + 2
+//     console.log(`This is how many spaces are available: ${30 - spaces} `);
+// } while (spaces <= 30)
+// console.log("Parking spot if full now.");
---
+++ js/while.js
@@ -0,0 +1,15 @@
+// num = 2
+// while (num <= 65536) {
+//     num = num * num;
+//     console.log("Number now is: " + num);
+// }
+
+
+//ice cream seller needs to sell all cones
+//generate a random number between 50 to 100.. math.floor
+//number represents how much cones to sell before start of loop
+//
+
+var allCones = Math.floor(Math.random() *50) + 50; // 50-100
+Math.floor(Math.random() * 5) + 1; // 1-5
+
