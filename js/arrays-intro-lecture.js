1f327c3b97659338dbd0c062395fa4c8f36a8b2e
Erik Ayala <ayala.erik61@gmail.com>
2021-04-26 17:01:21 -0500

    pushed exercises for today

---
+++ js/arrays-intro-lecture.js
@@ -0,0 +1,102 @@
+(function () {
+    "use strict";
+
+    //array- []
+    // var myArr = ["dog","cat", "fish"] // <-- each comma-delimited value is an element
+    // console.log(Array.isArray(myArr));
+    //
+
+    // console.log(myCharacteristics.length);
+    // first element in an array has an index of 0
+    // the last element in an array has an index of (length - 1)
+    // access elements with str[] method
+    // var myCharacteristics = ["Erik", "Student", "27"]; // 0, 1, 2
+    // console.log(`Hello my name is ${myCharacteristics[0]}, I am a ${myCharacteristics[1]}, and I am ${myCharacteristics[2]}!`);
+
+//TO DO: Mini Exercise
+//     function whatIWillEatToday (myFood) {
+//         var myFood = ["pizza", "hot dog", "tacos", "burger", "beer"]
+//         for (let i = 0; i <= myFood.length; i++) {
+//             console.log(`Today I will eat ${myFood[2]}`)
+//         }
+//     }
+//
+// whatIWillEatToday();
+
+    //For Each function
+
+
+    //TODO:
+    // .length - SUPER IMPORTANT
+    // .forEach() - ALSO IMPORTANT
+    // FOR loop always important!
+
+    // var days = ["monday,", "tuesday", "wednesday",];
+    // console.log(days.length)
+    // var newDays = days.push("thursday")
+    // console.log(newDays)
+    //
+    // console.log()
+
+
+    //  TODO: MINI-EXERCISE
+    //   -> var days = ["Monday", "Tuesday", "Wednesday"];
+    //   -> Create an array with the remaining days of the week
+    //   -> Call .forEach() on that NEW array
+    //   -> Inside the inline function you pass into .forEach():
+    //       -> add the remainingDay of the week to the old array
+    //   -> When the loop completes, console.log() the completed day of the week array
+
+
+
+        var remainingDays = ["Tuesday", "Wednesday", "Thursday", "Friday"]
+
+        remainingDays.unshift("Monday")
+
+        remainingDays.forEach(function (name) {
+            console.log(name)
+        })
+
+
+
+    // TODO - MINI EXERCISE
+//  -> Let's reverse engineer our days of the week array
+//  -> Make sure to console.log in order to see your code's behavior!
+//  -> Write a function which takes in your complete days of the week array
+//      -> It will eventually return your reverse-engineered array
+//  -> Using a FOR LOOP (not .forEach())
+//      -> pop() each item off the days array
+//      -> use the returned value from pop() to unshift the element
+//      -> on to the new array
+//  -> HINT: You may find that the loop doesn't behave as expected
+//      -> console.log EVERY possible thing which could change as your loop runs
+//          -> consider how the methods you are using change state of the array
+
+    // console.log(remainingDays);
+    //
+    //     function reverseEng(input) {
+    //         input;
+    //         var newDays = input.pop()
+    //
+    //         for (var i = 0; i < input.length +1; i++) {
+    //             console.log(input)
+    //             input.pop()
+    //             i = 0
+    //         }
+    //         console.log()
+    //     }
+    //
+    //     reverseEng(remainingDays);
+
+
+    
+
+
+
+
+
+
+
+
+
+}) ();
--- js/while.js
+++ js/while.js
@@ -1,17 +1,55 @@
-// num = 2
-// while (num <= 65536) {
-//     num = num * num;
-//     console.log("Number now is: " + num);
-// }
+(function () {
+//-----------------------------------
+    // 2
+    // 4
+    // 8
+    // 16
+    // 32
+    // 64
+    // 128
+    // 256
+    // 512
+    // 1024
+    // 2048
+    // 4096
+    // 8192
+    // 16384
+    // 32768
+    // 65536
 
+
+    // var x = 2
+    // while (x <= 65536) {
+    //     console.log(x);
+    //     x *= 2;
+    // }
+
+
+
+//-------------------------------------------
 
 //ice cream seller needs to sell all cones
 //generate a random number between 50 to 100.. math.floor
 //number represents how much cones to sell before start of loop
+// inside of loop random number between 1 - 5
+    // how many cones are being bought by her clients
+    
 //
 //
-// var allCones = Math.floor(Math.random() *50) + 50; // 50-100
-// Math.floor(Math.random() * 5) + 1; // 1-5
-//
-//
-//
+//         var allCones = Math.floor(Math.random() *50) + 50;// 50-100
+//         function getWantedCones(){
+    //         return Math.floor(Math.random() * 5) + 1;
+    //     }
+    //
+    // do {
+    //     var wantedCones = getWantedCones();
+    //     if (wantedCones > allCones) {
+    //         console.log(`I'm sorry, you wanted ${wantedCones}, but we only have ${allCones}`)
+    //         continue;
+    //     } while (allCones > 0) {
+    //
+    //     }
+
+
+
+})();
