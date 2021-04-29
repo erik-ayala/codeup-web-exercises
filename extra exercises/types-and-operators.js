2f37868de09d5c6f2c34c4cba6e822aa610d0b69
Erik Ayala <ayala.erik61@gmail.com>
2021-04-29 16:24:59 -0500

    renamed few exercises

---
+++ extra exercises/types-and-operators.js
@@ -0,0 +1,81 @@
+(function (){
+    "use strict";
+
+// function isOdd(input) {
+//     if (input % 2 === 1) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+// isOdd(2);
+
+
+// function isTrue(input) {
+//     if (Boolean(input)) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+
+// isTrue();
+// console.log(isTrue(9));
+
+// function isFalse(input) {
+//     if (Boolean(input) !== true) {
+//         console.log("its false");
+//     } else {
+//         console.log("its true");
+//     }
+// }
+//
+//     console.log(isFalse(0));
+
+
+
+// function subtract(input1, input2) {
+//     return input1 - input2;
+// }
+
+// console.log(subtract());
+
+
+// function subtract (a, b) {
+//     var total = (a - b);
+//     if (isNaN(a) || isNaN(b)) {
+//         return false;
+//     } else {
+//         return Number(total);
+//     }
+// }
+//
+//     console.log(subtract("8", null));
+
+
+// function toUppercase(input) {
+//     var upUp = input.toUpperCase();
+//     var thisIs = Number(input)
+//
+//     if (thisIs ) {
+//         return false;
+//     } else {
+//         return upUp;
+//     }
+// }
+//
+//     console.log(toUppercase("15"));
+
+
+function mockingCase(input) {
+    var res = "";
+    for (var i = 0; i < input.length; i++) {
+        res += i % 2 === 0 ? input.charAt(i).toUpperCase() : input.charAt(i);
+        }
+    return res;
+}
+
+    console.log(mockingCase("im da best"));
+
+
+})()
