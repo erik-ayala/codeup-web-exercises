dbc967bc67a819be4fb0782bfbc2fee8b8b84991
Erik Ayala <ayala.erik61@gmail.com>
2021-05-13 11:56:45 -0500

    pushed before lunch

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,24 +1,24 @@
 (function () {
 
-    function showMultiplicatonTable(num) {
-        var num;
-        for (var i = 1; i <= 10; i++) {
-            var answer = i * num
-            console.log(`${num} x ${i} = ${answer}`);
-        }
-    }
-    showMultiplicatonTable(8);
-
-
-
-    for(var x = 1; x <= 10; x++) {
-        var randomNumber = Math.floor(Math.random() * 20) + 200;
-        if (randomNumber % 2 === 0) {
-            console.log(`${randomNumber} is Even.`)
-        } else {
-            console.log(`${randomNumber} is Odd.`)
-        }
-    }
+    // function showMultiplicatonTable(num) {
+    //     var num;
+    //     for (var i = 1; i <= 10; i++) {
+    //         var answer = i * num
+    //         console.log(`${num} x ${i} = ${answer}`);
+    //     }
+    // }
+    // showMultiplicatonTable(8);
+    //
+    //
+    //
+    // for(var x = 1; x <= 10; x++) {
+    //     var randomNumber = Math.floor(Math.random() * 20) + 200;
+    //     if (randomNumber % 2 === 0) {
+    //         console.log(`${randomNumber} is Even.`)
+    //     } else {
+    //         console.log(`${randomNumber} is Odd.`)
+    //     }
+    // }
 
     //----------------------------------------
     // 1
@@ -31,24 +31,37 @@
     // 88888888
     // 999999999
 
-    let pyramidNumLog = () => {
-        for (var i = 1; i <= 9; i++) {
-            let addOne = '';
+    // let pyramidNumLog = () => {
+    //     for (var i = 1; i <= 9; i++) {
+    //         let addOne = '';
+    //         for (let x = 1; x <= i; x++) {
+    //             addOne += String(i);
+    //         }
+    //         console.log(addOne)
+    //     }
+    // }
+    // pyramidNumLog();
+
+    function pyramindSymbol(input) {
+        for (let i = 1; i <= input; i++) {
+            var addOne = '';
             for (let x = 1; x <= i; x++) {
                 addOne += String(i);
+                var star = addOne
             }
-            console.log(addOne)
+        console.log(addOne)
         }
     }
-    pyramidNumLog();
+pyramindSymbol(5);
+
 
 //-------------------------------------
-    for(var i = 100; i >= 1; i--) {
-        if (i % 5 === 0) {
-            console.log(i);
-        } else {
-        }
-    }
+//     for(var i = 100; i >= 1; i--) {
+//         if (i % 5 === 0) {
+//             console.log(i);
+//         } else {
+//         }
+//     }
 
 
 
