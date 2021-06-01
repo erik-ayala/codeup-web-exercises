5eb249af56f8c6b3d07be8c51f30a7b1532683d2
Erik Ayala <ayala.erik61@gmail.com>
2021-06-01 14:45:26 -0500

    pushed exercise

--- html-lecture-practice/dom-events.js
+++ html-lecture-practice/dom-events.js
@@ -1,16 +1,23 @@
-addButtonEvent()
+// addButtonEvent()
+//
+//
+// function addButtonEvent() {
+//     document.getElementById("submitBtn")
+//         .addEventListener("click", addPost)
+// }
+//
+// function addPost() {
+//     let blogpost = document.getElementById('blog-container');
+//     let paragraph = document.createElement("p");
+//     let blogDiv = document.getElementById('user-input');
+//     blogpost.appendChild(paragraph)
+//     paragraph.innerText = blogDiv.value
+//
+// }
 
-
-function addButtonEvent() {
-    document.getElementById("submitBtn")
-        .addEventListener("click", addPost)
+function add () {
+    for(var i = 0; i <= 10; i++) {
+        console.log(i);
+    }
 }
-
-function addPost() {
-    let blogpost = document.getElementById('blog-container');
-    let paragraph = document.createElement("p");
-    let blogDiv = document.getElementById('user-input');
-    blogpost.appendChild(paragraph)
-    paragraph.innerText = blogDiv.value
-
-}
+add()
