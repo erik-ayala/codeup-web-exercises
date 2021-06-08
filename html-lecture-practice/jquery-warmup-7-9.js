83732b04dfdbd027974d26c84ceb1942c16c5276
Erik Ayala <ayala.erik61@gmail.com>
2021-06-08 17:44:36 -0500

    pushed daily work

---
+++ html-lecture-practice/jquery-warmup-7-9.js
@@ -0,0 +1,28 @@
+// $('#submitBtn').click(function () {
+//     if ($(".form-control").length === 0) {
+//         alert("Form Cannot be Empty")
+//     } else {
+//         console.log($('.form-control'))
+//     }
+// });
+
+
+// let gamingBoi = {
+//     game: "minecraft",
+//     console: "pc",
+//     yearsGaming: 20,
+//     snacksAteWhileGaming: ["alcohol", "pizza", "tacos", "monsters"]
+// }
+//
+// console.log(gamingBoi)
+// console.log(gamingBoi.yearsGaming)
+
+
+$.ajax("https://jsonplaceholder.typicode.com/posts", {
+    type: "POST",
+    data: {
+        name: "ekal",
+        location: "bofa",
+    }
+});
+
